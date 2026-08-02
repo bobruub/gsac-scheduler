5693b67fb594fde2970ea8dba99e5a243e172ac1
Bob <bruun@gmail.com>
2026-08-02 17:49:33 +1000

    fixed variable file location

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+//const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // const FILE_PATH = path.join(__dirname, 'data', 'schedule.json');
 const SCHEDULE_FILE_NAME = process.env.SCHEDULE_FILE_NAME;
@@ -25,6 +27,8 @@ app.use(express.static('public'));
 
 // GET current schedule
 app.get('/api/schedule', (req, res) => {
+    console.log(`Reading schedule file from path: ${FILE_PATH}`);
+
   fs.readFile(FILE_PATH, 'utf8', (err, data) => {
     if (err) {
       return res.status(500).json({ error: 'Failed to read schedule file' });
@@ -39,6 +43,7 @@ app.get('/api/schedule', (req, res) => {
 
 // POST to overwrite schedule file
 app.post('/api/schedule', (req, res) => {
+    console.log(`Writing schedule file to path: ${FILE_PATH}`);
   const newSchedule = req.body;
   if (!Array.isArray(newSchedule)) {
     return res.status(400).json({ error: 'Payload must be an array of schedule items' });
